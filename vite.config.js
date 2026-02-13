433d731f349b755ebe2aaca61bcf952d2c7b4c48
clementnemelc <clement810038@gmail.com>
2026-02-14 03:36:57 +0800

    修正部署路徑與樣式

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/silkonix-web/", // ⚠️ 這行非常重要，前後都要有斜線，且名稱必須與 Repository 一致
+});
